e5b71cdf8e07148a836ba4e20007dadd6d3807e4
LuiiVasquez <luivasque95@gmail.com>
2022-10-24 15:15:46 -0600

    global objects - timers - CommonJS modules

---
+++ faztWeb/module/myModule.js
@@ -0,0 +1,14 @@
+const myWebAddress = "vasquezluis.com";
+const myNumber = 23;
+const myArray = [10, 20, 30];
+const user = {
+  name: "Ryan",
+  lastname: "Ray",
+};
+
+module.exports = {
+  myWebAddress,
+  myNumber,
+  myArray,
+  user,
+};
